a46d5a7d596b8b485b4e07a854d6023d749d9f47
Sergi Linares <slinares@omatech.com>
2022-11-04 14:04:17 +0100

    more color test testing

--- tests/Draughts/Types/ColorTest.php
+++ tests/Draughts/Types/ColorTest.php
@@ -6,10 +6,13 @@ use Hondilla\Draughts\Types\Color;
 use Hondilla\Draughts\Types\Coordinate;
 use PHPUnit\Framework\TestCase;
 
+//use function Hamcrest\Core\Is;
+
 class ColorTest extends TestCase
 {
     public function testGet()
     {
+        //$this->assertThat(Color::get(0), is(Color::WHITE));
         $this->assertEquals(Color::WHITE, Color::get(0));
         $this->assertEquals(Color::BLACK, Color::get(1));
         $this->assertEquals(Color::NULL, Color::get(2));
@@ -23,4 +26,35 @@ class ColorTest extends TestCase
         $this->assertEquals(Color::NULL, Color::getInitialColor(new Coordinate(3, 4)));
         $this->assertEquals(Color::NULL, Color::getInitialColor(new Coordinate(4, 5)));
     }
+
+    public function testGivenNullThenEmptyInitial()
+    {
+        $this->assertEquals(' ', Color::NULL->getInitial());
+    }
+
+    public function testGivenBlackThenInitialIsLowerCaseB()
+    {
+        $this->assertEquals('b', Color::BLACK->getInitial());
+    }
+
+    public function testGivenWhiteThenInitialIsLowerCaseW()
+    {
+        $this->assertEquals('w', Color::WHITE->getInitial());
+    }
+
+    public function testGivenNullThenIsNull()
+    {
+        $this->assertTrue(Color::NULL->isNull());
+    }
+
+    public function testGivenBlackThenOppositeIsWhite()
+    {
+        $this->assertEquals(Color::WHITE, Color::BLACK->opposite());
+    }
+
+    public function testGivenWhiteThenOppositeIsBlack()
+    {
+        $this->assertEquals(Color::BLACK, Color::WHITE->opposite());
+    }
+
 }
